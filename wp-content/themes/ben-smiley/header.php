b108ecfc888c47abbdba5b826a385df2f3041fb3
Mahima Manu <mahima@ajency.in>
2014-07-16 18:11:10 +0530

    new bensmiley theme

--- wp-content/themes/ben-smiley/header.php
+++ wp-content/themes/ben-smiley/header.php
@@ -158,10 +158,15 @@ if($using_secondary == 'header_with_secondary') { ?>
 							<a id="mobile-cart-link" href="<?php echo $woocommerce->cart->get_cart_url(); ?>"><i class="icon-salient-cart"></i></a>
 						<?php } 
 					} ?>
+
+                <?php
+
+                if ( is_user_logged_in() ):
+                    $userdata = wp_get_current_user();?>
 						
 					<!--admin signin part start-->
 					
-     <!--<div  class="pull-right m-l-20">
+     <div  class="pull-right m-l-20">
                 <div class="user-profile pull-left">
                     <img src="http://1.gravatar.com/avatar/5ba2411c7d1e867264c0dcec5a160a46?s=64&amp;d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D64&amp;r=G" alt="" width="35" height="35" id="user-photo">
                 </div>
@@ -169,33 +174,35 @@ if($using_secondary == 'header_with_secondary') { ?>
                     <li class="quicklinks btn">
                         <a data-toggle="dropdown" class="dropdown-toggle  pull-right " href="javascript:void(0)" id="user-options">
                             <div class="pull-left">
-                                <span class=" display_name">admin</span>
+                                <span class=" display_name"><?php echo $userdata->display_name;?></span>
                             </div>
                         <span class="sf-sub-indicator" style="height: 16px;"><i class="icon-angle-down"></i></span>
                         </a>
-						<ul id="popup" class="dropdown-menu  pull-right" role="menu" aria-labelledby="user-options"> 
-							<li> 
-								<a href="#logout" id="logout"> <i class="fa fa-power-off"></i>&nbsp;&nbsp;Log Out </a> 
-							</li> 
+						<ul id="popup" class="dropdown-menu  pull-right" role="menu" aria-labelledby="user-options">
+							<li>
+								<a href="#logout" id="logout"> <i class="fa fa-power-off"></i>&nbsp;&nbsp;Log Out </a>
+							</li>
 						</ul>
                     </li>
                 </ul>
-            </div>-->
-					<!--admin signin part ends--><div class="signup"><div><a href="">signup</a></div></div>
-					<nav class="alignright ">
-						<ul class="sf-menu">
-							<?php 
-							if(has_nav_menu('top_nav')) {
-							    wp_nav_menu( array('walker' => new Nectar_Arrow_Walker_Nav_Menu, 'theme_location' => 'top_nav', 'container' => '', 'items_wrap' => '%3$s' ) ); 
-							}
-							else {
-								echo '<li><a href="">No menu assigned!</a></li>';
-							}
-							?>
-							<li id="search-btn"><div><a href=""><span class="icon-salient-search" aria-hidden="true"></span></a></div></li>
-							
-						</ul>
-					</nav>
+            </div>
+                <?php endif; ?>
+			<div class="signup"><div><a href="home">signup</a></div></div>
+            <!--admin signin part ends-->
+            <nav class="alignright ">
+                <ul class="sf-menu">
+                    <?php
+                    if(has_nav_menu('top_nav')) {
+                        wp_nav_menu( array('walker' => new Nectar_Arrow_Walker_Nav_Menu, 'theme_location' => 'top_nav', 'container' => '', 'items_wrap' => '%3$s' ) );
+                    }
+                    else {
+                        echo '<li><a href="">No menu assigned!</a></li>';
+                    }
+                    ?>
+                    <li id="search-btn"><div><a href=""><span class="icon-salient-search" aria-hidden="true"></span></a></div></li>
+
+                </ul>
+            </nav>
 					
 					
 				</div><!--/span_9-->
