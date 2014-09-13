5981b902da1bfe6ae88667a847688ad6196ca928
Suraj Air <surajair@live.com>
2014-09-13 12:00:42 +0530

    side bar issue fixed

--- wp-content/themes/binpress/functions.php
+++ wp-content/themes/binpress/functions.php
@@ -53,12 +53,8 @@ add_action( 'after_setup_theme', 'binpress_theme_setup' );
 function arphabet_widgets_init() {
 
     register_sidebar( array(
-        'name' => 'Home right sidebar',
-        'id' => 'home_right_1',
-        'before_widget' => '<div>',
-        'after_widget' => '</div>',
-        'before_title' => '<h2 class="rounded">',
-        'after_title' => '</h2>',
+        'name' => 'Page Sidebar',
+        'id' => 'sidebar-2'
     ) );
 }
 add_action( 'widgets_init', 'arphabet_widgets_init' );
