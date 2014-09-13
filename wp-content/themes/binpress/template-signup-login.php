cddcab55692f9d2bb2feb9d2ccd3259c32decaaa
Jeromie Fernandes <jeromie@ajency.in>
2014-09-13 16:09:39 +0530

    issue #41 done

--- wp-content/themes/binpress/template-signup-login.php
+++ wp-content/themes/binpress/template-signup-login.php
@@ -79,9 +79,9 @@ get_header();
                     </div>
                     <div class="row">
                         <div class="col-md-10">
-                            <button class="btn btn-primary ladda-button"
+                            <button class="btn btn-primary btn-cons ladda-button pull-right"
                             data-style="expand-left" id="btn-signup">
-                            <span class="ladda-label">Submit</span>
+                            <span class="ladda-label">Sign Up</span>
                             </button>
                             <span class="ajax-loader-signup" style="display: none"></span>
                             <input type="reset" id="btn-signup-form-reset" style="display: none"/>
@@ -148,50 +148,51 @@ get_header();
 
             </div>
             <br>
+        </div>
 
-            <!--FORGOT PASSWORD MODAL START -->
-            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-                 aria-hidden="true">
-                <div class="modal-dialog fgt_pass" style="top:25%;">
-                    <div class="modal-content">
-                        <div class="text-center padding-10">
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                            <i class="icon-credit-card icon-7x"></i>
-                            <h4 id="myModalLabel" class="semi-bold">Forgot Password?</h4>
+        <!--FORGOT PASSWORD MODAL START -->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+             aria-hidden="true">
+            <div class="modal-dialog fgt_pass" style="top:25%;">
+                <div class="modal-content">
+                    <div class="text-center padding-10">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                        <i class="icon-credit-card icon-7x"></i>
+                        <h4 id="myModalLabel" class="semi-bold">Forgot Password?</h4>
 
-                            <p class="no-margin">No problem! Enter your email address here to receive a link to change
-                                password.</p>
-                        </div>
-                        <div class="tiles white padding-20">
-                            <form id="forgot-password-form" class="login-form">
-							 <div id="display-forgot-msg"></div>
-                                <div class="row form-row">
-                                    <div class="col-md-12">
-                                        <div class="input-with-icon  right">
-                                            <i class=""></i>
-                                            <input type="text" class="form-control" placeholder="Email"
-                                                   name="userEmail" id="userEmail">
-                                        </div>
+                        <p class="no-margin">No problem! Enter your email address here to receive a link to change
+                            password.</p>
+                    </div>
+                    <div class="tiles white padding-20">
+                        <form id="forgot-password-form" class="login-form">
+                         <div id="display-forgot-msg"></div>
+                            <div class="row form-row">
+                                <div class="col-md-12">
+                                    <div class="input-with-icon  right">
+                                        <i class=""></i>
+                                        <input type="text" class="form-control" placeholder="Email"
+                                               name="userEmail" id="userEmail">
                                     </div>
                                 </div>
-                                <div class="m-t-10">
-                                    <button type="button" class="btn btn-primary btn-cons"
-                                            id="btn-forgot-pass">Submit
-                                    </button>
+                            </div>
+                            <div class="m-t-10">
+                                <button type="button" class="btn btn-primary btn-cons"
+                                        id="btn-forgot-pass">Submit
+                                </button>
 
-                                    <input type="reset" id="btn-forgot-form-reset" style="display: none"/>
+                                <input type="reset" id="btn-forgot-form-reset" style="display: none"/>
 
 
-                                </div>
-                            </form>
-                        </div>
-                        <!-- /.modal-content -->
+                            </div>
+                        </form>
                     </div>
-                    <!-- /.modal-dialog -->
+                    <!-- /.modal-content -->
                 </div>
+                <!-- /.modal-dialog -->
             </div>
-            <!--FORGOT PASSWORD MODAL END -->
         </div>
+        <!--FORGOT PASSWORD MODAL END -->
+
     </div>
     <!-- END CONTAINER -->
 
