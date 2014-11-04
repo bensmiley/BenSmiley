e08821d72a454f701e21f5e33817acd2b8075a24
Nutan Kamat <nutan@ajency.in>
2014-11-04 13:49:07 +0530

    cancel pending subscription test

--- wp-content/themes/binpress/PHPModules/braintree_webhook/functions.php
+++ wp-content/themes/binpress/PHPModules/braintree_webhook/functions.php
@@ -34,12 +34,6 @@ subscription_active_email($user_name,$user_email, $new_subscription_id);
 
 function bt_subscription_cancelled($new_subscription_id,$customer_details){
 
-	$to = "nutankamat769@gmail.com";
-	$subject = "Subscription is cancelled";
-	$content = "Your subscription".$new_subscription_id." is cancelled successfully and your emails id is ".$customer_details['email'];
-
-	$status = wp_mail($to, $subject, $content);
-
 	if(empty($customer_details['email'])){
 		$domain_id = get_domain_for_bt_subscription($new_subscription_id);
 		$domain_user_id = get_post_field( 'post_author', $domain_id );
@@ -51,6 +45,12 @@ function bt_subscription_cancelled($new_subscription_id,$customer_details){
 		$user_email = $customer_details['email'];
 		$user_name = $customer_details['name'];		
 	}
+
+	$to = "nutankamat769@gmail.com";
+	$subject = "Subscription is cancelled";
+	$content = "Your subscription".$new_subscription_id." is cancelled successfully and your emails id is ".$user_email;
+
+	$status = wp_mail($to, $subject, $content);
 
 
 	subscription_canceled_email($user_name,$user_email, $new_subscription_id);
